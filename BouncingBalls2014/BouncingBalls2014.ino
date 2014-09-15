e60d80433e63720411b3276e30a57f292ca4da92
Danny Wilson <dlwilson@dannysmokretina.home>
2014-09-15 22:33:21 +0530

    First upload of the BouncingBalls2014 code
    Make those bouncy balls bounce!

---
+++ BouncingBalls2014/BouncingBalls2014.ino
@@ -0,0 +1,68 @@
+//  BouncingBalls2014 is a program that lets you animate an LED strip
+//  to look like a group of bouncing balls
+//
+//  Daniel Wilson, 2014
+//
+//  With BIG thanks to the FastLED community!
+///////////////////////////////////////////////////////////////////////////////////////////
+#include "FastLED.h"
+
+#define NUM_LEDS          60
+#define DATA_PIN          3
+#define COLOR_ORDER       GRB
+#define CHIPSET           NEOPIXEL
+#define BRIGHTNESS        255
+#define GRAVITY           -9.81              // Downward (negative) acceleration of gravity in m/s^2
+#define h0                1                  // Starting height, in meters, of the ball (strip length)
+#define NUM_BALLS         3                  // Number of bouncing balls you want (recommend < 7, but 20 is fun in its own way)
+
+float h[NUM_BALLS] ;                         // An array of heights
+float vImpact0 = sqrt( -2 * GRAVITY * h0 );  // Impact velocity of the ball when it hits the ground if "dropped" from the top of the strip
+float vImpact[NUM_BALLS] ;                   // As time goes on the impact velocity will change, so make an array to store those values
+float tCycle[NUM_BALLS] ;                    // The time since the last time the ball struck the ground
+int   pos[NUM_BALLS] ;                       // The integer position of the dot on the strip (LED index)
+long  tLast[NUM_BALLS] ;                     // The clock time of the last ground strike
+float COR[NUM_BALLS] ;                       // Coefficient of Restitution (bounce damping)
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  delay(2000);                               // sanity check delay - allows reprogramming if accidently blowing power w/leds
+  FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  LEDS.setBrightness(BRIGHTNESS);
+  Serial.begin(115200);
+  for (int i = 0 ; i < NUM_BALLS ; i++) {    // Initialize variables
+    tLast[i] = millis();
+    h[i] = h0;
+    pos[i] = 0;                              // Balls start on the ground
+    vImpact[i] = vImpact0;                   // And "pop" up at vImpact0
+    tCycle[i] = 0;
+    COR[i] = 0.90 - float(i)/pow(NUM_BALLS,2); 
+  }
+}
+
+void loop() {
+  for (int i = 0 ; i < NUM_BALLS ; i++) {
+    tCycle[i] =  millis() - tLast[i] ;     // Calculate the time since the last time the ball was on the ground
+
+    // A little kinematics equation calculates positon as a function of time, acceleration (gravity) and intial velocity
+    h[i] = 0.5 * GRAVITY * pow( tCycle[i]/1000 , 2.0 ) + vImpact[i] * tCycle[i]/1000;
+
+    if ( h[i] < 0 ) {                      
+      h[i] = 0;                            // If the ball crossed the threshold of the "ground," put it back on the ground
+      vImpact[i] = COR[i] * vImpact[i] ;   // and recalculate its new upward velocity as it's old velocity * COR
+      tLast[i] = millis();
+
+      if ( vImpact[i] < 0.01 ) vImpact[i] = vImpact0;  // If the ball is barely moving, "pop" it back up at vImpact0
+    }
+    pos[i] = round( h[i] * (NUM_LEDS - 1) / h0);       // Map "h" to a "pos" integer index position on the LED strip
+  }
+
+  //Choose color of LEDs, then the "pos" LED on
+  for (int i = 0 ; i < NUM_BALLS ; i++) leds[pos[i]] = CHSV( uint8_t (i * 40) , 255, 255);
+  FastLED.show();
+  //Then off for the next loop around
+  for (int i = 0 ; i < NUM_BALLS ; i++) {
+    leds[pos[i]] = CRGB::Black;
+  }
+}
